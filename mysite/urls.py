c9b79c692746e409cc90012c4a66c11cb3ca0fc7
Craig <indigomx9@gmail.com>
2017-08-26 18:21:10 -0400

    Setup the templates, urls, and views.

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,11 +1,9 @@
 """"""
 from django.conf.urls import url, include
 from django.contrib import admin
-from blog import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^blog/', include('blog.urls', namespace='blog', app_name='blog')),
-    url(r'', include('blog.urls')),
+    url(r'^blog/', include('blog.urls', namespace='blog', app_name='blog')),    
 ]
 
